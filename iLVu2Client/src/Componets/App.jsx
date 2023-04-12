b174e6a423dc8298341e832f7fd36a59e5b73cea
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-12 18:06:42 -0500

    libraries: added daisyUI

--- iLVu2Client/src/Componets/App.jsx
+++ iLVu2Client/src/Componets/App.jsx
@@ -1,9 +1,14 @@
 export default function App() {
   return (
 
-
-      <h1 className={` text-9xl font-bold text-blue-600 flex justify-center items-start `}>
+      <div>
+            <h1 className={` text-9xl font-bold text-blue-600 flex justify-center items-start `}>
       iLVu2
     </h1>
+      <button class="btn btn-primary">Button</button>
+
+      </div>
+  
+    
   )
 }
