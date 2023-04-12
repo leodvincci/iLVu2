6e4c68832f7a110d62f87ca3780bf759d6a1f780
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-12 18:32:48 -0500

    [library]:installed react icons

--- iLVu2Client/src/Componets/App.jsx
+++ iLVu2Client/src/Componets/App.jsx
@@ -8,14 +8,34 @@ import HomePage from "../Pages/HomePage"
 export default function App() {
   return (
 
+    
+
 
     <BrowserRouter>
+
+
+<div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost normal-case text-xl">iLVu2  </a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Journal Prompt</a></li>
+ 
+      <li><a>Mood Check-In</a></li>
+      <li><a>Logout</a></li>
+   
+
+
+    </ul>
+  </div>
+</div>
+
+
     <Routes>
       <Route path="" element={<HomePage/>}/>
       <Route path="/user/login/" element={<LoginPage/>}/>
       <Route path="/user/register/" element={<Registration/>}/>
-
-
     </Routes>
     </BrowserRouter>
 
