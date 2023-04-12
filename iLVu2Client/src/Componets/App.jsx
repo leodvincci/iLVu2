acce3a063f5b01c6216651bc6bdf6b1255b9d5b7
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-12 13:19:27 -0500

    [library]: added daisyUI kit

--- iLVu2Client/src/Componets/App.jsx
+++ iLVu2Client/src/Componets/App.jsx
@@ -1,6 +1,8 @@
 export default function App() {
   return (
-      <h1 className={`text-9xl font-bold text-blue-600 flex justify-center items-start `}>
+
+
+      <h1 className={` text-9xl font-bold text-blue-600 flex justify-center items-start `}>
       iLVu2
     </h1>
   )
