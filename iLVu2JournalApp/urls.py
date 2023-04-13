6c7050a9e0b33c004724aa3eebecdfbe066414bf
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-12 20:39:59 -0500

    api: created register user api

--- iLVu2JournalApp/urls.py
+++ iLVu2JournalApp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("user/login", views.index, name="index"),
     path("user/register", views.index, name="register"),
+    path("user/register_user",views.register_user, name="register_user")
 
 ]
--- iLVu2JournalApp/views.py
+++ iLVu2JournalApp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-
+from rest_framework.decorators import api_view
 
 # Create your views here.
 def index(request):
     the_index = open("static/index.html")
     return HttpResponse(the_index)
+
+@api_view(["POST"])
+def register_user(request):
+    return HttpResponse(f"{request.data} Has Been Registerd!")
