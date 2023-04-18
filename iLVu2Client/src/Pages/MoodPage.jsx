c3c9e871658a17fc175558b3c3b5c63dcc18d61f
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-18 09:46:34 -0500

    styles: updated ui for mood page

--- iLVu2Client/src/Pages/MoodPage.jsx
+++ iLVu2Client/src/Pages/MoodPage.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React from "react";
-import.meta.env.PROD
 
 export  default function MoodPage(){
     const [moodEmojis, setMoodEmoji] = React.useState()
@@ -21,22 +20,24 @@ export  default function MoodPage(){
 
 
         return (
-            <div className={`flex flex-col items-center `}>
+            <div className={`flex flex-col items-center h-screen w-screen justify-center `}>
 
-                <div className={`flex flex-row w-3/4 h-screen flex-wrap p-10`}>
+                <div className={`flex flex-row w-1/4 p-10 h-[20rem] flex-wrap overflow-scroll items-center justify-center`}>
                     {
                       moodEmojis !== undefined ?
                           moodEmojis[0].map(e =>{
-                                  // if(e.slug.includes("face")){
                                        return (
-                                      <h1 onClick={()=>{alert(e.slug)}} className={`text-[12rem] hover:text-[15rem] hover:cursor-pointer m-9`}>{e.character}</h1>
+                                      <h1 onClick={()=>{alert(e.slug)}} className={`text-[5rem] hover:text-[7rem] hover:cursor-pointer p-9`}>{e.character}</h1>
                                   )
-                              // }
-
                           })
                           : ""
                     }
                 </div>
+                    <div>
+                        <textarea className="textarea textarea-accent m-8" placeholder="Why Do You Feel...?"></textarea>
+                    </div>
+                                        <button className="btn btn-accent">Submit</button>
+
             </div>
 
         )
