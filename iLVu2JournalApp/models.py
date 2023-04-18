a64546cd9a4f76e0645a0fc2aa3aef7e25bb952d
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-18 10:49:45 -0500

    database: created and updated models for prompt category,siteprompts, user prompts,and prompt responses

--- iLVu2JournalApp/models.py
+++ iLVu2JournalApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class PromptCategory(models.Model):
+    category_name = models.CharField(max_length=30)
+    category_description = models.TextField(max_length=200)
+    category_img = models.TextField(max_length=100, null=True)
+
+
+class SitePrompt(models.Model):
+    prompt_text = models.CharField(max_length=200)
+    Prompt_Category = models.ForeignKey(PromptCategory, on_delete=models.CASCADE)
+
+
+class UserPrompt(models.Model):
+    prompt_text = models.CharField(max_length=200)
+    Prompt_Category = models.ForeignKey(PromptCategory, on_delete=models.CASCADE)
+
+
+class PromptResponse(models.Model):
+    prompt_type = models.CharField(max_length=30)
+    prompt_response_text = models.TextField(max_length=200)
