bdb95fc11e75716ee5718076df22f3c9889b72bc
Leo D. Penrose <lpenrose@depaul.edu>
2023-04-26 00:06:17 -0500

    ui/ux: added prompt question to start of prompt responses page

--- iLVu2Client/src/Pages/JournalPromptResponsePage.jsx
+++ iLVu2Client/src/Pages/JournalPromptResponsePage.jsx
@@ -6,11 +6,15 @@ import axios from "axios";
 
 export default function JournalPromptPage(){
     const [catData, setCatData] = React.useState([])
-      const { site_prompt_id } = useParams();
+    const [promptData, setPromptData] = React.useState("")
+
+    const { catid } = useParams();
+    const { site_prompt_id } = useParams();
 
     console.log("CatData: ",catData)
-    console.log("CatID: ",site_prompt_id)
-
+    console.log("CatID: ",catid)
+    console.log("Site-Prompt-id : ",site_prompt_id)
+    console.log("Some Data : ",promptData)
 
 
     React.useEffect(()=>{
@@ -20,10 +24,33 @@ export default function JournalPromptPage(){
             })
     },[])
 
+
+    React.useEffect(()=>{
+        axios.get(`http://localhost:8000/api/v1/siteprompt?id=${catid}`)
+            .then((res)=>{
+                console.log("HELLO")
+                for(const r of res.data.Data){
+                    if(r.id == site_prompt_id){
+                        setPromptData(r)
+                        console.log("YUP")
+                    }
+                }
+            })
+    },[])
+
+
+
+
     return(
         <div >
 
-                  <div className={`flex flex-row flex-wrap`}>
+                  <div className={`flex flex-col flex-wrap`}>
+
+                      <div className="chat chat-start">
+  <div className="chat-bubble chat-bubble-primary">{promptData.prompt_text}</div>
+</div>
+
+
                 {catData.map(d=>{
                     return (
                         <div>
